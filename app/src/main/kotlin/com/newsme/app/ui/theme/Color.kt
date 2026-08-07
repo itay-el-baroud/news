0d1835daf44813ea3dde454e85d86317b625fa24
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-08 01:45:43 +0300

    Add color definitions for app theme

---
+++ app/src/main/kotlin/com/newsme/app/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.newsme.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF1E88E5)
+val PrimaryDark = Color(0xFF1565C0)
+val Secondary = Color(0xFF424242)
+val Tertiary = Color(0xFF757575)
+val Background = Color(0xFFFAFAFA)
+val Surface = Color(0xFFFFFFFF)
+val Error = Color(0xFFD32F2F)
+val Success = Color(0xFF388E3C)
+val Warning = Color(0xFFF57C00)
+
+val DarkPrimary = Color(0xFF64B5F6)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkSecondary = Color(0xFFBDBDBD)
